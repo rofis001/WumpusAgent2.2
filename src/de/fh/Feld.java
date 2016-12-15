c10d1b07b4cdacad608a6160f8ef6d3c91b37879
rofis001 <rofis001@stud.fh-dortmund.de>
2016-12-15 11:09:57 +0100

    just format

--- src/de/fh/Feld.java
+++ src/de/fh/Feld.java
@@ -6,22 +6,47 @@ import de.fh.connection.Vector2;
  * Created by Brian on 07.12.2016.
  */
 public class Feld {
-    private boolean visited=false;
-    private byte trapdanger=0;
-    private Vector2 coords;
-    private boolean wall=false;
+	private boolean visited = false;
+	private byte trapdanger = 0;
+	private Vector2 coords;
+	private boolean wall = false;
 
-    public Feld(){}
-    public Feld(Vector2 c)
-    {
-        coords=c;
-    }
-    public boolean IsVisited(){ return visited;}
-    public byte GetTrapdanger(){return trapdanger;}
-    public Vector2 GetCoords(){return coords;}
-    public boolean IsWall(){return wall;}
-    public void SetVisited(){visited=true;}
-    public void SetTrapdanger(byte p){trapdanger=p;}
-    public void SetCoords(int x, int y){coords.set(x,y);}
-    public void SetWall(){wall=true;}
+	public Feld() {
+	}
+
+	public Feld(Vector2 c) {
+		coords = c;
+	}
+
+	public boolean IsVisited() {
+		return visited;
+	}
+
+	public byte GetTrapdanger() {
+		return trapdanger;
+	}
+
+	public Vector2 GetCoords() {
+		return coords;
+	}
+
+	public boolean IsWall() {
+		return wall;
+	}
+
+	public void SetVisited() {
+		visited = true;
+	}
+
+	public void SetTrapdanger(byte p) {
+		trapdanger = p;
+	}
+
+	public void SetCoords(int x, int y) {
+		coords.set(x, y);
+	}
+
+	public void SetWall() {
+		wall = true;
+	}
 }
